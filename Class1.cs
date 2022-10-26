59e87c702fef0e985ef1e397179c2eae81b802bf
ashwin333 <ashwinpatil832@gmail.com>
2022-10-26 13:32:20 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDemo
+{
+     public class Date  //--> members of class data member,method,properties,constructor,event
+     {
+        //variables,data members
+        private int day, year;
+        private string month;
+        //create a method to assign value to data member
+
+        public void Add(int day, string month, int year)
+        {
+            this.day = day; //this refers to data member
+            this.month = month;
+            this.year = year;
+        }
+        // method to retrieve data
+
+        public string Print()
+        {
+            return $"Date: {day}/{month}/{year}";
+        }
+     }
+
+}
+    
+
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using FirstDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_demo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create object of Date class
+            Date d1 = new Date(); // new allocate memory
+            d1.Add(8, "Oct", 2022);
+         Console.WriteLine(d1.Print());
+
+
+          Date d2 = new Date();
+          d2.Add(9, "Oct", 2022);
+          Console.WriteLine(d2.Print());
+
+        }
+    }
+}
